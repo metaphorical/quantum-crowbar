78e897eea0f267057330820bad53f9a079f8ec89
Rastko Vukasinovic <rastko@vast.com>
2015-04-16 10:11:48 +0200

    Add performance analysis, prepare for yslow implementation

--- modules/performance-analysis.js
+++ modules/performance-analysis.js
@@ -8,20 +8,8 @@ var toFile = require('../plugins/to-file.js');
 var phantomPath = './node_modules/.bin/phantomjs';
 var confessPath = './plugins/confess/confess.js';
 
-var performanceAnalysis = function(config) {
-    console.log("Performance analyses started".yellow);
-    var pages = config.pages;
-
-    _.each(pages, function(page) {
-        var pageLink = config.hosts[process.argv[2]] + page.link;
-        var childArgs = [
-                      confessPath,
-                      pageLink,
-                      'appcache'
-                    ];
-        console.log("\nGenerating appcache manifest of:".bold);
-        console.log(page.name.yellow + "\n");
-        var confessAppcache = childProcess.execFile(phantomPath, childArgs, function(err, stdout, stderr) {
+var runPhantomChildProcess = function(args) {
+    var phantomProc = childProcess.execFile(phantomPath, args, function(err, stdout, stderr) {
             if (err) {
                 console.log("Error happend".red);
                 console.log("err: ", err.red);
@@ -30,10 +18,41 @@ var performanceAnalysis = function(config) {
                 console.log("Successfully finished".green);
             }
         });
+    return phantomProc;
+};
+var performanceAnalysis = function(config) {
+    console.log("Performance analyses started".yellow);
+    var pages = config.pages;
+
+    _.each(pages, function(page) {
+        var pageLink = config.hosts[process.argv[2]] + page.link;
+        var appcacheArgs = [
+                      confessPath,
+                      pageLink,
+                      'appcache'
+                    ];
+        var performanceArgs = [
+                      confessPath,
+                      pageLink,
+                      'performance'
+                    ];
+        console.log("\nGenerating appcache manifest of:".bold);
+        console.log(page.name.yellow + "\n");
+        var confessAppcache = runPhantomChildProcess(appcacheArgs);
         // Detaching child process event loop from parents event loop
         confessAppcache.unref();
+
         confessAppcache.stdout.on('data', function(data) {
-            toFile('./reports/cq-' + page.name +'.appcache', data.toString());
+            toFile('./reports/cq-' + config.hosts[process.argv[2]].replace(/[^\w]/g,'') + '-' + page.name +'.appcache', data.toString());
+        });
+        console.log("\nGenerating performance report for:".bold);
+        console.log(page.name.yellow + "\n");
+        var confessPerf = runPhantomChildProcess(performanceArgs);
+        // Detaching child process event loop from parents event loop
+        confessPerf.unref();
+
+        confessPerf.stdout.on('data', function(data) {
+            toFile('./reports/cq-' + config.hosts[process.argv[2]].replace(/[^\w]/g,'') + '-' + page.name +'-performance.log', data.toString());
         });
     });
 };
